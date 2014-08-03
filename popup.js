76e6efb5ebc2f2d938cb97c22ed8d7172617781b
ota42y <ota42y@gmail.com>
2014-08-04 07:36:36 +0900

    last update load

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
-console.log("button");
-console.log($("#ReloadButton"));
-
 $(document).ready( function() {
   $("#ReloadButton").click(function(){
     console.log("clicked")
-    target = document.getElementById("last_update");
+    var target = document.getElementById("last_update");
     target.innerHTML = "Click";
+  });
+
+  // if last update is exist, set data
+  chrome.storage.local.get("last_update",function(result){
+    var value = result["last_update"];
+    if (value){
+      var target = document.getElementById("last_update");
+      target.innerHTML = value;
+    }
   });
 });
